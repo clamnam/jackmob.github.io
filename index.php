c866642f447350aa2fe0c11a71c634800efd1fc5
clamnam <jacktruso@gmail.com>
2022-09-26 00:51:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css -->
+    <link rel="stylesheet" href="styles.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,200;0,300;0,400;0,500;0,600;1,400&family=Space+Mono:ital,wght@0,200;0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;1,300;1,400&family=Quattrocento+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"> 
+
+    <title>jackmob.ie</title>
+</head>
+<body>       
+    <!-- sidebar -->
+    <div class="sidenav"><h1 class="navItem">Jack Moloney O'Brien</h1>
+        <ul>
+        <li><a href="https://github.com/clamnam" class="navItem">github</a></li>
+        <li><a href="mailto:myoneplusdot@gmail.com" class="navItem">mail</a></li>
+    </ul>
+</div>
+    <div class="header">
+
+        <!-- main -->
+    <div class="main">
+        <div class="content">
+            <h4>This website serves to journal my progress from the genesis of this site onwards. </h4>
+            <div class="post">
+                <p>
+                    During my first year of college I have learned much of web design, javascript(p5.js) and java(processing). My progress was good and I felt like I was improving alot but due to working and life my progress stagnated entirely so now that the summer months are coming towards an end I am going to try and warm back up on my skills and regain and grow them once again. 
+                </p>
+                <p class="date">01/09/2022</p>
+            </div>
+            <div class="post">
+                <p>
+                    During my first year of college I have learned much of web design, javascript(p5.js) and java(processing). My progress was good and I felt like I was improving alot but due to working and life my progress stagnated entirely so now that the summer months are coming towards an end I am going to try and warm back up on my skills and regain and grow them once again. 
+                </p>
+                <p class="date">01/09/2022</p>
+            </div>
+        </div>
+    </div>
+
+    
+    </div>
+</body>
+</html>
